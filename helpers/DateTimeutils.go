1b63055ac1e4cb7a73bce54e5b3312ded4554a5a
hoangnvhybrid <hoangnv@hybrid-technologies.vn>
2020-03-26 08:56:33 +0700

    them file xu ly ngay thang

---
+++ helpers/DateTimeutils.go
@@ -0,0 +1,9 @@
+package helpers
+
+import "time"
+
+func ConvertTimeToLocal() time.Time {
+	now := time.Now()
+	rounded := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	return rounded
+}
