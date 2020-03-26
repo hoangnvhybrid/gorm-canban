a6a454978b7b768797cd0b16855b0bf80d8de3e2
hoangnvhybrid <hoangnv@hybrid-technologies.vn>
2020-03-26 09:04:21 +0700

    add function timenow(0

--- helpers/DateTimeutils.go
+++ helpers/DateTimeutils.go
@@ -7,3 +7,6 @@ func ConvertTimeToLocal() time.Time {
 	rounded := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	return rounded
 }
+func TimeNow() time.Time {
+	return time.Now()
+}
